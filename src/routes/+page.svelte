98c73d7e1d661a559db827f9a3f23ede7401f07b
TheBolduc <97337225+TheBolduc@users.noreply.github.com>
2023-04-22 16:31:11 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import ChatMessage from '$lib/components/ChatMessage.svelte';
+  import ChatBotOption from '$lib/components/ChatBotOption.svelte';
   import type { ChatCompletionRequestMessage } from 'openai';
   import { SSE } from 'sse.js';
 
@@ -43,7 +44,7 @@
         if (e.data === '[DONE]') {
           chatMessages = [...chatMessages, { role: 'assistant', content: answer }];
           answer = '';
-          eventSource.close(); // Close the connection
+          eventSource.close();
           loading = false;
           inputField.disabled = false;
           scrollToDiv.scrollIntoView({ behavior: 'smooth' });
@@ -69,6 +70,21 @@
     inputField.disabled = false;
     console.error(err);
   }
+
+  let showChatInterface = false;
+  const chatBotNames = [
+    'Chatbot 1',
+    'Chatbot 2',
+    'Chatbot 3',
+    'Chatbot 4',
+    'Chatbot 5',
+    'Chatbot 6',
+  ];
+
+  function selectChatBot(name) {
+    console.log(`Selected ${name}`);
+    showChatInterface = true;
+  }
 </script>
 
 <style>
@@ -80,53 +96,65 @@
 <main>
   <div class="chatbot-wrapper">
     <div class="flex flex-col min-h-screen pt-4 w-full px-8 items-center gap-2">
-      <div class="flex flex-col w-full max-w-screen-md bg-white p-6 rounded-lg shadow-lg">
-        <div class="flex flex-col items-center justify-center mb-4">
-          <h1 class="text-2xl font-bold w-full text-center">BusinessGPT</h1>
-          <p class="text-sm italic">
-            Your own personal business analyst and assistant.
-          </p>
-          <div class="w-full border-b border-gray-400 mb-6"></div>
-        </div>
-        <div class="flex flex-col pt-4 w-full h-full px-8 items-center gap-2">
-          <div class="flex flex-col gap-2 h-[calc(100vh-300px)] overflow-y-auto mt-4 mb-4" bind:this={chatContainer}>
-            <ChatMessage
-              type="assistant"
-              message="Hi! My name is Tommy, ask me any business-related questions, I'm here to help you grow your business, learn about business, start your business and much more!"
-            />
-            {#each chatMessages as message}
-              <ChatMessage type={message.role} message={message.content} />
-            {/each}
-            {#if answer}
-              <ChatMessage type="assistant" message={answer} />
-            {/if}
-            {#if loading && !answer}
-              <ChatMessage type="assistant" message="Loading.." />
-            {/if}
+      {#if !showChatInterface}
+        <section class="...">
+          {#each chatBotNames as name}
+            <ChatBotOption {name} onSelect={() => selectChatBot(name)} />
+          {/each}
+        </section>
+      {/if}
+
+      {#if showChatInterface}
+        <div class="chat-interface">
+          <div class="flex flex-col w-full max-w-screen-md bg-white p-6 rounded-lg shadow-lg">
+            <div class="flex flex-col items-center justify-center mb-4">
+              <h1 class="text-2xl font-bold w-full text-center">BusinessGPT</h1>
+              <p class="text-sm italic">
+                Your own personal business analyst and assistant.
+              </p>
+              <div class="w-full border-b border-gray-400 mb-6"></div>
+            </div>
+            <div class="flex flex-col pt-4 w-full h-full px-8 items-center gap-2">
+              <div class="flex flex-col gap-2 h-[calc(100vh-300px)] overflow-y-auto mt-4 mb-4" bind:this={chatContainer}>
+                <ChatMessage
+                  type="assistant"
+                  message="Hi! My name is Tommy, ask me any business-related questions, I'm here to help you grow your business, learn about business, start your business and much more!"
+                />
+                {#each chatMessages as message}
+                  <ChatMessage type={message.role} message={message.content} />
+                {/each}
+                {#if answer}
+                  <ChatMessage type="assistant" message={answer} />
+                {/if}
+                {#if loading && !answer}
+                  <ChatMessage type="assistant" message="Loading.." />
+                {/if}
+              </div>
+              <div class="" bind:this={scrollToDiv} />
+            </div>
+            <form
+              class="flex w-full rounded-md bg-gray-200 py-6 px-2 shadow-inner space-x-2"
+              on:submit|preventDefault={() => handleSubmit()}
+            >
+              <input
+                type="text"
+                class="input input-bordered w-full shadow bg-white text-black"
+                bind:this={inputField}
+                bind:value={query}
+                placeholder="Ask me anything about business..."
+                disabled={loading}
+              />
+              <button
+                type="submit"
+                class="btn btn-primary bg-green-500 hover:bg-green-600 border border-green-500 hover:border-green-600"
+                disabled={loading}
+              >
+                Send
+              </button>
+            </form>
           </div>
-          <div class="" bind:this={scrollToDiv} />
         </div>
-        <form
-          class="flex w-full rounded-md bg-gray-200 py-6 px-2 shadow-inner space-x-2"
-          on:submit|preventDefault={() => handleSubmit()}
-        >
-          <input
-            type="text"
-            class="input input-bordered w-full shadow bg-white text-black"
-            bind:this={inputField}
-            bind:value={query}
-            placeholder="Ask me anything about business..."
-            disabled={loading}
-          />
-          <button
-            type="submit"
-            class="btn btn-primary bg-green-500 hover:bg-green-600 border border-green-500 hover:border-green-600"
-            disabled={loading}
-          >
-            Send
-          </button>
-        </form>
-      </div>
+      {/if}
     </div>
   </div>
 </main>
