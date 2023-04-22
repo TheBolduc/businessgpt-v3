492ea9cf6bc4b8704d9098ff6b801c05ebe4390b
TheBolduc <97337225+TheBolduc@users.noreply.github.com>
2023-04-22 16:35:07 +0200

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,16 @@
+<!-- src/routes/+layout.svelte -->
 <script>
-	import '../app.css'
+	import '../app.css';
+	import Sidebar from '$lib/components/Sidebar.svelte';
 </script>
 
 <div class="flex flex-col items-center max-w-2xl mx-auto">
-	<slot />
+	<Sidebar />
+	<main class="w-full">
+		<slot />
+	</main>
 </div>
+
+<style>
+	/* Add styles for the layout container */
+</style>
