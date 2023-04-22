8d29222377179ac0cb1f397ec4c90d248023a386
TheBolduc <97337225+TheBolduc@users.noreply.github.com>
2023-04-22 18:16:51 +0200

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,26 +1,26 @@
 <!-- src/routes/+layout.svelte -->
 <script>
-  import '../app.css';
-  import Sidebar from '$lib/components/Sidebar.svelte';
-  import { goto } from '$app/navigation';
+	import '../app.css';
+	import Sidebar from '$lib/components/Sidebar.svelte';
+	import { goto } from '$app/navigation';
 </script>
 
 <header>
-  <nav class="flex justify-between items-center">
-    <a href="/">
-      <img class="h-16" src="https://i.imgur.com/BTYJrKC.png" alt="BusinessGPT logo" />
-    </a>
-    <button class="btn" on:click={() => goto('/')}>Go Home</button>
-  </nav>
+	<nav class="flex justify-between items-center">
+		<a href="/">
+			<img class="h-16" src="https://i.imgur.com/BTYJrKC.png" alt="BusinessGPT logo" />
+		</a>
+		<button class="btn" on:click={() => goto('/')}>Go Home</button>
+	</nav>
 </header>
 
 <div class="flex flex-col items-center max-w-2xl mx-auto">
-  <Sidebar />
-  <main class="w-full">
-    <slot />
-  </main>
+	<Sidebar />
+	<main class="w-full">
+		<slot />
+	</main>
 </div>
 
 <style>
-  /* Add styles for the layout container */
+	/* Add styles for the layout container */
 </style>
