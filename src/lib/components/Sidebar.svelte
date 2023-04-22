a6ec76fda7a2195f5695388a1541ee9ab8a51746
TheBolduc <97337225+TheBolduc@users.noreply.github.com>
2023-04-22 16:32:01 +0200

    Create Sidebar.svelte

---
+++ src/lib/components/Sidebar.svelte
@@ -0,0 +1,19 @@
+<!-- src/lib/components/Sidebar.svelte -->
+<script>
+  import {goto} from '$app/navigation';
+
+  function navigateToHomePage() {
+    goto('/');
+  }
+</script>
+
+<aside class="...">
+  <button on:click={navigateToHomePage} class="...">
+    Go to Home
+  </button>
+  <!-- Add more navigation items if needed -->
+</aside>
+
+<style>
+  /* Add styles for the sidebar */
+</style>
