a67a00d6823dacd4c056d35a74c705b85269901f
TheBolduc <97337225+TheBolduc@users.noreply.github.com>
2023-04-22 18:14:21 +0200

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 <script>
 	import '../app.css';
 	import Sidebar from '$lib/components/Sidebar.svelte';
+	import { goto } from '$app/navigation';
 </script>
 
 <div class="flex flex-col items-center max-w-2xl mx-auto">
