776286c9d118c7a6700777b0c893f557172e2517
TheBolduc <97337225+TheBolduc@users.noreply.github.com>
2023-04-22 16:25:49 +0200

    Create ChatBotOption.svelte

---
+++ src/lib/components/ChatBotOption.svelte
@@ -0,0 +1,13 @@
+<!-- src/lib/components/ChatBotOption.svelte -->
+<script>
+  export let name = "";
+  export let onSelect = () => {};
+</script>
+
+<button on:click={onSelect} class="...">
+  {name}
+</button>
+
+<style>
+  /* Add styles for the ChatBotOption button */
+</style>
